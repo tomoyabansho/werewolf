86853f8792d09a03720151ab8cdd30afd872ec0b
tomoyabansho <bansho@seesaa.co.jp>
2018-10-11 19:35:00 +0900

    implement fragment news class

---
+++ app/src/main/java/jp/co/seesaa/intern/werewolf/data/News.kt
@@ -0,0 +1,3 @@
+package jp.co.seesaa.intern.werewolf.data
+
+data class News(val title: String, val message: String, val imageSource: String)
--- app/src/main/java/jp/co/seesaa/intern/werewolf/view/NewsFragment.kt
+++ app/src/main/java/jp/co/seesaa/intern/werewolf/view/NewsFragment.kt
@@ -1,10 +1,38 @@
 package jp.co.seesaa.intern.werewolf.view
 
+import android.arch.lifecycle.ViewModelProviders
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import jp.co.seesaa.intern.werewolf.R
+import jp.co.seesaa.intern.werewolf.R.string.news_subscription_message
+import jp.co.seesaa.intern.werewolf.R.string.news_title_message
+import jp.co.seesaa.intern.werewolf.data.News
+import jp.co.seesaa.intern.werewolf.databinding.FragmentNewsBinding
+import jp.co.seesaa.intern.werewolf.model.NewsModel
 
 class NewsFragment : Fragment() {
+
+    private lateinit var model: NewsModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        model = ViewModelProviders.of(this).get(NewsModel::class.java)
+
+        model.news.postValue(News(getString(news_title_message), getString(news_subscription_message), ""))
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        val binding = DataBindingUtil.inflate<FragmentNewsBinding>(inflater, R.layout.fragment_news, container, false)
+
+        binding.setLifecycleOwner(this)
+        binding.model = model
+
+        return binding.root
     }
 }
