a0ecfe604ee7a1b411cbe3c9f4e6ac62e2524c3b
tomoyabansho <bansho@seesaa.co.jp>
2018-10-11 14:40:38 +0900

    use timber

--- app/src/main/java/jp/co/seesaa/intern/werewolf/App.kt
+++ app/src/main/java/jp/co/seesaa/intern/werewolf/App.kt
@@ -1,9 +1,28 @@
 package jp.co.seesaa.intern.werewolf
 
 import android.app.Application
+import android.util.Log
+import timber.log.Timber
+import timber.log.Timber.DebugTree
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        } else {
+            Timber.plant(WerewolfCrashReportingTree())
+        }
+    }
+
+    private class WerewolfCrashReportingTree : Timber.Tree() {
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            if (priority == Log.ERROR) {
+                Log.e("Crash Report", "***** Fatal Error *****")
+                Log.d("Crash Report", tag)
+                Log.d("Crash Report", message)
+            }
+        }
     }
 }
