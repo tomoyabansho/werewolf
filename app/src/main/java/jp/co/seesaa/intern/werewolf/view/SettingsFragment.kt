e9b05514202851ebf7e841a5d6a25936639c49fe
tomoyabansho <bansho@seesaa.co.jp>
2018-11-01 13:09:23 +0900

    use preference activity

--- app/src/main/java/jp/co/seesaa/intern/werewolf/view/SettingsFragment.kt
+++ app/src/main/java/jp/co/seesaa/intern/werewolf/view/SettingsFragment.kt
@@ -1,31 +1,11 @@
 package jp.co.seesaa.intern.werewolf.view
 
-import android.arch.lifecycle.ViewModelProviders
-import android.databinding.DataBindingUtil
 import android.os.Bundle
-import android.support.v4.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.preference.PreferenceFragmentCompat
 import jp.co.seesaa.intern.werewolf.R
-import jp.co.seesaa.intern.werewolf.data.UserData
-import jp.co.seesaa.intern.werewolf.databinding.FragmentSettingsBinding
-import jp.co.seesaa.intern.werewolf.model.SettingsModel
 
-class SettingsFragment : Fragment() {
-    lateinit var model: SettingsModel
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        model = ViewModelProviders.of(this).get(SettingsModel::class.java)
-        model.data.postValue(UserData(getString(R.string.preference_test_id), getString(R.string.preference_test_name), 5, 2, 3))
-    }
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        super.onCreateView(inflater, container, savedInstanceState)
-        val binding = DataBindingUtil.inflate<FragmentSettingsBinding>(inflater, R.layout.fragment_settings, container, false)
-        binding.setLifecycleOwner(this)
-        binding.model = model
-        return binding.root
+class SettingsFragment : PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferences)
     }
 }
